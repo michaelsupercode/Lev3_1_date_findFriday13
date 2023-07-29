a8bd8cd571eb3b9657d53052a13713e8fde0c117
Michael Hanel <michaelsupercode@gmail.com>
2023-07-29 16:57:56 +0200

    rev

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,7 @@ findFriday13 = (year) => {
         if (date.getDay() == 5) {
             const listItem = document.createElement('li');
             counter++;
-            heading.textContent = `das Jahr ${year} hat ${counter} Unglückstage.`;
+            heading.textContent = `das Jahr ${year} hat ${counter} fuckin' Unglückstag(e)`;
             document.body.appendChild(heading);
             document.body.appendChild(list);
             listItem.textContent = date;
@@ -19,8 +19,21 @@ findFriday13 = (year) => {
     }
 }
 
+findFriday13(2025);
+findFriday13(2024);
+findFriday13(2023);
+findFriday13(2022);
+findFriday13(2021);
 findFriday13(2020);
+findFriday13(2019);
+findFriday13(2018);
+findFriday13(2017);
 findFriday13(2016);
 findFriday13(2015);
+findFriday13(2014);
+findFriday13(2013);
+findFriday13(2012);
+findFriday13(2011);
+findFriday13(2010);
 
 console.log(new Date(2020, 1, 14));
